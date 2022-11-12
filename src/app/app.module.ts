02607e40883679b535bc96abb671d837cfc93ea3
Kelvin Ajayi <89777325+Marvel-Deone@users.noreply.github.com>
2022-11-12 09:02:27 +0000

    Change the directory of some file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './public/menu/menu.component';
-import { HomeComponent } from './home/home.component';
+// import { MenuComponent } from './public/menu/menu.component';
 import { authGuardServices } from 'auth/auth-guard.service';
-import { FooterComponent } from './public/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+// import { FooterComponent } from './public/footer/footer.component';
 import { SigninComponent } from './signin/signin.component';
+import { FooterComponent } from './website/footer/footer.component';
+import { MenuComponent } from './website/menu/menu.component';
 
 @NgModule({
   declarations: [
